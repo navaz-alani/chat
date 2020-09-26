6d26e5e957280123aac623571ad80fed36e14c6f
Navaz Alani <nalani@uwaterloo.ca>
2020-09-26 12:24:00 -0400

    set up react, nextjs, ts, tailwind, postcss

---
+++ web/ui/pages/_app.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import '../styles/tailwind.css';
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps } : AppProps) {
+  return (
+    <Component {...pageProps} />
+  );
+};
+
+export default MyApp;
